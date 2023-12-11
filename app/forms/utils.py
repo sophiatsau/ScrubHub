4bb6365f00f7f01703c97d6a54626481e0565b09
Sophia <sophia.tsau@gmail.com>
2023-12-10 21:58:04 -0800

    format address edit/create form

--- app/forms/utils.py
+++ app/forms/utils.py
@@ -4,7 +4,7 @@ from wtforms.validators import ValidationError
 # helper function for testing if valid US postal code
 def is_valid_us_zip(form, field):
    if not re.search("^\d{5}(-\d{4})?$", field.data):
-      raise ValidationError("Zip code is invalid.")
+      raise ValidationError("Zip code is in the wrong format (use XXXXX or XXXXX-XXXX)")
 
 
 #not working
