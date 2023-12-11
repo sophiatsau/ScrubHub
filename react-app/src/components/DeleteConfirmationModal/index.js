b47e881ac13caa07d11f1785c6de914057e96d31
Sophia <sophia.tsau@gmail.com>
2023-12-10 22:25:36 -0800

    delete modal css

--- react-app/src/components/DeleteConfirmationModal/index.js
+++ react-app/src/components/DeleteConfirmationModal/index.js
@@ -8,18 +8,18 @@ import { getFullAddress } from '../../store/utils'
 import "./DeleteConfirmationModal.css";
 
 export default function DeleteConfirmationModal({ deleteFunction, itemName}) {
-	const dispatch = useDispatch();
+	// const dispatch = useDispatch();
 	const {closeModal} = useModal()
 
 	return (
-		<>
+		<div className="delete-modal">
 		<h1>Confirm Deletion</h1>
         <p>Are you sure you want to delete your {itemName.toLowerCase()}?</p>
 
-        <div>
-            <button className="delete-button" onClick={deleteFunction}>Yes, Delete {itemName}</button>
-            <button className="keep-button" onClick={closeModal}>No, Keep {itemName}</button>
+        <div className="delete-modal-buttons">
+            <button className="delete-button purple-button" onClick={deleteFunction}>Yes, Delete {itemName}</button>
+            <button className="keep-button light-button" onClick={closeModal}>No, Keep {itemName}</button>
         </div>
-		</>
+		</div>
 	);
 }
