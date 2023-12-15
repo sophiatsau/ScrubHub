694a2ea47092181cfe0bcea2a4b6763573e6ebf7
Sophia <sophia.tsau@gmail.com>
2023-12-15 11:26:25 -0800

    removed old code

--- app/seeds/critters.py
+++ app/seeds/critters.py
@@ -2,45 +2,8 @@ from app.models import db, Critter, environment, SCHEMA
 from sqlalchemy.sql import text
 import json
 
-# critters info
-NAMES=["Turtle F", "Frilled Lizards 2yr M", "Snakes", "Frogs", "Kangaroo 1yr M"]
-SPECIES=["Chrysemys picta", "Chlamydosaurus Kingii", "Pantherophis guttatus", "Dryophytes cinereus", "Osphranter rufus"]
-CATEGORIES=["Reptiles","Reptiles","Reptiles","Amphibians", "Other Mammals"]
-PREVIEW_IMAGES=["https://crittr-images.s3.us-west-1.amazonaws.com/turtle.webp", None, None, "https://crittr-images.s3.us-west-1.amazonaws.com/hop-grid.jpg", "https://crittr-images.s3.us-west-1.amazonaws.com/hop-cover.jpg"]
-DESCRIPTIONS=["A little turtle", None, "Hsssss", "Green. Comes in all genders and ages.", "He definitely did not kick a toddler in the face when I took him grocery shopping last week"]
-
 # Adds critters
 def seed_critters():
-    # critters for demo user
-    # for i in range(3):
-    #     new_critter = Critter(
-    #         name=NAMES[i],
-    #         species=SPECIES[i],
-    #         shopId=1,
-    #         # userId=1,
-    #         price=(i+1)*15,
-    #         category=CATEGORIES[i],
-    #         previewImageUrl=PREVIEW_IMAGES[i],
-    #         description=DESCRIPTIONS[i],
-    #         stock=i*5,
-    #     )
-    #     db.session.add(new_critter)
-
-    # # seeders for shop 2
-    # for i in range(3,5):
-    #     new_critter = Critter(
-    #         name=NAMES[i],
-    #         species=SPECIES[i],
-    #         shopId=2,
-    #         # userId=2,
-    #         price=(i)*15,
-    #         category=CATEGORIES[i],
-    #         previewImageUrl=PREVIEW_IMAGES[i],
-    #         description=DESCRIPTIONS[i],
-    #         stock=[10,1][i-3],
-    #     )
-    #     db.session.add(new_critter)
-
     with open("app/seeds/data/critters.json", "r") as file:
         data = json.load(file)
         db.session.execute(db.insert(Critter.__table__).values(data))
