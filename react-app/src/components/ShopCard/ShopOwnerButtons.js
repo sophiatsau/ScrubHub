13e1f30688eaba571f34fbfc413ab4a1c6cecd84
Sophia <sophia.tsau@gmail.com>
2024-01-10 17:21:50 -0800

    created separate shop owner buttons component

--- react-app/src/components/ShopCard/ShopOwnerButtons.js
+++ react-app/src/components/ShopCard/ShopOwnerButtons.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import DeleteConfirmationModal from '../DeleteConfirmationModal';
+import OpenModalButton from '../OpenModalButton';
+import { useModal } from '../../context/Modal';
+import { thunkDeleteShop } from '../../store/shops';
+import { deleteUserShop } from '../../store/session';
 
 export default function ShopOwnerButtons({shop}) {
-  return (
+    const dispatch = useDispatch()
+    const {closeModal} = useModal()
+
+    const deleteShop = async () => {
+      const res = await dispatch(thunkDeleteShop(shop.id));
+
+      if (res.errors) {
+        alert(`${Object.values(res.errors).join(" ")} Please refresh the page and try again later.`)
+      } else {
+        dispatch(deleteUserShop(shop.id))
+      }
+
+      closeModal()
+    }
+
+    return (
     <div className="shop-owner-buttons">
         <Link to={`/profile/shops/${shop.id}/edit`}>
           <button className='purple-button'>Edit Shop</button>
