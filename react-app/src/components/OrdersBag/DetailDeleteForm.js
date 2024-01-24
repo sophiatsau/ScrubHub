09d56f0d664fc855c79e5923f13c6a318764775d
Sophia <sophia.tsau@gmail.com>
2024-01-24 14:16:47 -0800

    trashcan opens delete modal

---
+++ react-app/src/components/OrdersBag/DetailDeleteForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { useModal } from "../../context/Modal";
+
+export default function DeleteConfirmationModal({ deleteFunction, itemType, itemName}) {
+	// const dispatch = useDispatch();
+	const {closeModal} = useModal()
+
+	return (
+		<div className="delete-modal">
+		<h1>Confirm Deletion</h1>
+        <p>Are you sure you want to delete your {itemType.toLowerCase()}, <span className="bold">{itemName}</span>?</p>
+
+        <div className="delete-modal-buttons">
+            <button className="delete-button purple-button" onClick={deleteFunction}>Yes, Delete {itemType}</button>
+            <button className="keep-button light-button" onClick={closeModal}>No, Keep {itemType}</button>
+        </div>
+		</div>
+	);
+}
--- react-app/src/components/OrdersBag/OrderDetailCard.js
+++ react-app/src/components/OrdersBag/OrderDetailCard.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import DeleteConfirmationModal from '../DeleteConfirmationModal'
+import OpenModalButton from '../OpenModalButton'
 
 export default function OrderDetailCard({detail}) {
-  const deleteOrderDetail = console.log
+  const deleteOrderDetail = e => {
+    e.stopPropagation()
+    console.log("DELETE ORDER DETAIL")
+  }
 
   return (
     <>
         {detail.quantity}
-        <p className='purple'>{detail.critterName}</p>
-        <i className="fa-solid fa-trash-can" onClick={deleteOrderDetail}/>
+        <p className='purple' style={{justifySelf:"start"}}>{detail.critterName}</p>
+        <OpenModalButton
+          buttonText={<i className="fa-solid fa-trash-can"/>}
+          modalComponent={<DeleteConfirmationModal deleteFunction={deleteOrderDetail} itemType="critter" itemName={detail.critterName}/>}
+          className="trashcan"
+          onButtonClick={deleteOrderDetail}
+        />
+
         <p >{(detail.unitPrice * detail.quantity).toFixed(2)}</p>
     </>
   )
