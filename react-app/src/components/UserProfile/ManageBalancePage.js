77669a7dc69837296e77c9a99e45681d4beb9038
Sophia <sophia.tsau@gmail.com>
2024-01-25 18:36:28 -0800

    all icons, links for sites

--- react-app/src/components/UserProfile/ManageBalancePage.js
+++ react-app/src/components/UserProfile/ManageBalancePage.js
@@ -27,22 +27,25 @@ export default function ManageBalancePage() {
                         <img className="money-button-image" src="https://crittr-images.s3.us-west-1.amazonaws.com/LinkedIn_icon_circle.svg.png" alt="linkedin icon"/>
                     </button>
                 </a>
-                <a target="_blank" href="">
+                <a target="_blank" href="https://sophiatsau.github.io/">
                     <button className='add-money-button' onClick={increaseBalance}>
                         <div className='amount-added'>+ ${addMoney}</div>
                         <div>Check out my Portfolio Site</div>
+                        <img className="money-button-image" src="https://crittr-images.s3.us-west-1.amazonaws.com/browser-icon-bold.png" alt="portfolio icon"/>
                     </button>
                 </a>
-                <a target="_blank" href="">
+                <a target="_blank" href="https://spotifrog.onrender.com/">
                     <button className='add-money-button' onClick={increaseBalance}>
                         <div className='amount-added'>+ ${addMoney}</div>
-                        <div>Listen to music, upload albums, and build personal playlists on a frog-themed Spotify lookalike</div>
+                        <div>Listen to music, upload albums, and build playlists on a frog-themed Spotify lookalike</div>
+                        <img className="money-button-image" src="https://spotifrogmp3.s3.us-west-1.amazonaws.com/IMG_0034-1.png" alt="spotifrog icon"/>
                     </button>
                 </a>
-                <a target="_blank" href="">
+                <a target="_blank" href="https://infestation.onrender.com/">
                     <button className='add-money-button' onClick={increaseBalance}>
                         <div className='amount-added'>+ ${addMoney}</div>
                         <div>A Meetup clone for cockroaches to establish social groups and create events</div>
+                        <img className="money-button-image" src="https://64.media.tumblr.com/3cb9180466d6edf0203d313e97c04cdb/40017b7f673771aa-60/s2048x3072/b3536f24eff16ec0595e0ab4e5789634cf1b518e.jpg" alt="infestation icon"/>
                     </button>
                 </a>
             </div>
